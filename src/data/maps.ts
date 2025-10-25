1c38a850fa0b87ff89ae42e3fec71c5effc341c9
xuanzhi33 <xuanzhi33@qq.com>
2025-10-25 18:27:50 +0000

    feat: add new map and corresponding documentation for strings and print functionality

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -3,6 +3,7 @@ import text0 from "../data/0.md?raw"
 import text1 from "../data/1.md?raw"
 import text2 from "../data/2.md?raw"
 import text3 from "../data/3.md?raw"
+import text4 from "../data/4.md?raw"
 export default [
     {
         map: `
@@ -86,5 +87,26 @@ BBBBBBBBBBBBBBBBBBBB
             return pos.x === 19 && pos.y === 1;
         },
         info: text3
+    },
+    {
+
+        map: `
+____________________
+___________________F
+________________SSSS
+_____________SSSS___
+__________SSSS______
+_______SSSS_________
+____SSSS____________
+_SSSS_______________
+DDDDDDDDDDDDDDDDDDDD
+BBBBBBBBBBBBBBBBBBBB
+`,
+        start: { x: 0, y: 7 },
+        // goal: { x: 19, y: 1 },
+        goal(_1: { x: number; y: number }, _2: GridItem[][]) {
+            return false;
+        },
+        info: text4
     },
 ]
