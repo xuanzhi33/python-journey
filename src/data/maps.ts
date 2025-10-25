50a07a5e6f42d05ddc9e7cd85c265b113ccfcdf6
xuanzhi33 <xuanzhi33@qq.com>
2025-10-25 21:52:02 +0000

    feat: add new map and update goal logic for ore detection; include new lesson on variables and counting diamonds

--- src/data/maps.ts
+++ src/data/maps.ts
@@ -7,6 +7,7 @@ import text4 from "../data/4.md?raw"
 import text5 from "../data/5.md?raw"
 import text6 from "../data/6.md?raw"
 import text7 from "../data/7.md?raw"
+import text8 from "../data/8.md?raw"
 export default [
     {
         map: `
@@ -171,11 +172,55 @@ SSSSSSSSSSdSSSSSSSSS
 BBBBBBBBBBBBBBBBBBBB
 `,
         start: { x: 9, y: 2 },
-        goal(pos: { x: number; y: number }, _2: GridItem[][]) {
-            // Update goal logic to reflect the while loop concept from 6.md
-            return pos.x === 19 && pos.y === 7; // Steve reaches the flower
+        goal(_: { x: number; y: number }, gameState: GridItem[][]) {
+            for (let row of gameState) {
+                for (let item of row) {
+                    if (item.name?.endsWith("Ore")) {
+                        return false;
+                    }
+                }
+            }
+            return true;
         },
         info: text7
     },
+    {
+        map: `
+SSSSSSSSSSSSSSSSSSSS
+SSSSSSSSSSSSSSSSSSSS
+SSSSSSSSSSSSSSSSSSSS
+SSSSSSSSSSSSSSSSSSSS
+SSSSSSSSSSSSSSSSSSSS
+___________________B
+SSdSgSSdddSSdSgSdSgS
+SSSSSSSSSSSSSSSSSSSS
+SSSSSSSSSSSSSSSSSSSS
+BBBBBBBBBBBBBBBBBBBB
+`,
+        start: { x: 0, y: 5 },
+        goal(_: { x: number; y: number }, __: GridItem[][]) {
+            return false;
+        },
+        info: text8
+    },
+    {
+        map: `
+______LLL___________
+_____LLLLL_____LL___
+_____LLOLL____LLL___
+_______O______LOL___
+_______O__F____O___F
+GGGGGGGGGGGGGGGGGGGG
+ScSSSSSSScccSSSSSSiS
+SciiSSSSSSScSSSSSSSS
+ScSgSSgSSSSdSSSggSdS
+BBBBBBBBBBBBBBBBBBBB
+`,
+        start: { x: 0, y: 4 },
+        goal(_: { x: number; y: number }, __: GridItem[][]) {
+            return false;
+        },
+        info: text8
+    }
 
 ]
